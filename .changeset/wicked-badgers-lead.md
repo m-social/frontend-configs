e66dc03a490be867ac3c8f2d4c5fa4d06e852083
Artem Pitikin <a.pitikin@msocialproduction.com>
2026-02-18 22:07:51 +0300

    🐛 (oxlint): correct the names of rules for eslint integration

---
+++ .changeset/wicked-badgers-lead.md
@@ -0,0 +1,5 @@
+---
+"@m-social/oxlint-config": patch
+---
+
+Correct the names of rules for eslint integration
--- configs/oxlint/eslint/react.js
+++ configs/oxlint/eslint/react.js
@@ -11,31 +11,31 @@ export default {
 		// react/jsx-no-duplicate-props
 		"@eslint-react/jsx-no-duplicate-props": "off",
 		// react/jsx-no-target-blank
-		"@eslint-react/no-unsafe-target-blank": "off",
+		"@eslint-react/dom/no-unsafe-target-blank": "off",
 		// react/no-danger-with-children
-		"@eslint-react/no-dangerously-set-innerhtml-with-children": "off",
+		"@eslint-react/dom/no-dangerously-set-innerhtml-with-children": "off",
 		// react/no-direct-mutation-state
 		"@eslint-react/no-direct-mutation-state": "off",
 		// react/no-find-dom-node
-		"@eslint-react/no-find-dom-node": "off",
+		"@eslint-react/dom/no-find-dom-node": "off",
 		// react/no-render-return-value
-		"@eslint-react/no-render-return-value": "off",
+		"@eslint-react/dom/no-render-return-value": "off",
 		// react/no-string-refs
 		"@eslint-react/no-string-refs": "off",
 		// react/void-dom-elements-no-children
-		"@eslint-react/no-void-elements-with-children": "off",
+		"@eslint-react/dom/no-void-elements-with-children": "off",
 		// react/no-array-index-key
 		"@eslint-react/no-array-index-key": "off",
 		// react/iframe-missing-sandbox
-		"@eslint-react/no-missing-iframe-sandbox": "off",
+		"@eslint-react/dom/no-missing-iframe-sandbox": "off",
 		// react/no-script-url
-		"@eslint-react/no-script-url": "off",
+		"@eslint-react/dom/no-script-url": "off",
 		// react/jsx-no-comment-textnodes
 		"@eslint-react/jsx-no-comment-textnodes": "off",
 		// react/no-danger
-		"@eslint-react/no-dangerously-set-innerhtml": "off",
+		"@eslint-react/dom/no-dangerously-set-innerhtml": "off",
 		// react/button-has-type
-		"@eslint-react/no-missing-button-type": "off",
+		"@eslint-react/dom/no-missing-button-type": "off",
 		// react/self-closing-comp
 		"@stylistic/jsx-self-closing-comp": "off",
 	},
